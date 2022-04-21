bda95adfa5de75594436aae3db3e101709fb445e
Oskar Szura <oskar.szura@gmail.com>
2022-04-21 10:35:16 +0200

    CODA-250 - Menu navigation

--- main/main.ino
+++ main/main.ino
@@ -1,33 +1,22 @@
+#include "menu.h";
 #include <Arduino.h>
-#include <U8g2lib.h>
-#include <Wire.h>
-
-U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE, D2, D1);
 
 int lsState = LOW;
 int rsState = LOW;
+Menu *menu;
 
 void setup(void) {
-  pinMode(D3, INPUT);
-  pinMode(D4, INPUT);
-  u8g2.begin();
+  pinMode(D6, INPUT);
+  pinMode(D7, INPUT);
+
+  menu = new Menu();
 }
 
 void loop(void) {
-  lsState = digitalRead(D3);
-  rsState = digitalRead(D4);
+  lsState = digitalRead(D6);
+  rsState = digitalRead(D7);
 
-  u8g2.clearBuffer();
-  u8g2.setFont(u8g2_font_ncenB08_tr);
+  menu->render(lsState, rsState);
 
-  if (lsState && !rsState) {
-    u8g2.drawStr(0, 10, "Left clicked");
-  } else if (rsState && !lsState) {
-    u8g2.drawStr(0, 10, "Right clicked");
-  } else {
-    u8g2.drawStr(0, 10, "Main menu");
-  }
-
-  u8g2.sendBuffer();
   delay(100);
 }
---
+++ main/views/counter.h
@@ -0,0 +1,21 @@
+
+#ifndef H_COUNTER_VIEW
+#define H_COUNTER_VIEW
+
+#include <U8g2lib.h>
+#include "../virtual/view.h"
+
+class CounterView: public AbstractView {
+public:
+    U8G2 *u8g2;
+
+    CounterView(U8G2 *u8g2) {
+        this->u8g2 = u8g2;
+    }
+
+    void render(int isLeftBtnPressed, int isRightBtnPressed) {
+        this->u8g2->drawStr(0, 10, "Counter");
+    }
+};
+
+#endif /* H_COUNTER_VIEW */
