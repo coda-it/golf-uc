fb9068da37c20ca7a4ecb5f510eac771488e2fbe
Oskar Szura <oskar.szura@gmail.com>
2022-07-25 21:10:18 +0200

    CODA-254 - WiFi credentials input

--- main/views/settings.h
+++ main/views/settings.h
@@ -13,6 +13,8 @@ public:
   AbstractRepository *repository;
   WifiDataSource *wifi;
   int status;
+  std::string ssid = "A";
+  std::string password = "A";
 
   SettingsView(U8G2 *u8g2, AbstractRepository *repository) {
     this->u8g2 = u8g2;
@@ -20,20 +22,51 @@ public:
   }
 
   void render(State *state) {
-    std::string content[3] = {"Settings",
-                              "connect = " + std::to_string(this->status)};
+    const int ssidLength = this->ssid.length();
+    std::string network =
+        "Network: " +
+        (ssidLength >= 2 ? this->ssid.substr(0, ssidLength - 1) : "") + "[" +
+        this->ssid[ssidLength - 1] + "]";
+
+    const int passwordLength = this->password.length();
+    std::string password =
+        "Password: " +
+        (passwordLength >= 2 ? this->password.substr(0, passwordLength - 1)
+                             : "") +
+        "[" + this->password[passwordLength - 1] + "]";
+
+    std::string content[4] = {"Settings", network, password,
+                              "Connect: " + std::to_string(this->status)};
 
     if (state->level == 0) {
       content[0] = ">" + content[0];
     } else if (state->level == 1) {
       if (state->rightButton) {
-        this->status = this->wifi->connect();
+        this->ssid.push_back('A');
+      }
+      if (state->leftButton) {
+        this->ssid[this->ssid.length() - 1]++;
       }
 
       content[1] = ">" + content[1];
+    } else if (state->level == 2) {
+      if (state->rightButton) {
+        this->password.push_back('A');
+      }
+      if (state->leftButton) {
+        this->password[this->password.length() - 1]++;
+      }
+
+      content[2] = ">" + content[2];
+    } else if (state->level == 3) {
+      if (state->rightButton) {
+        this->status = this->wifi->connect();
+      }
+
+      content[3] = ">" + content[3];
     }
 
-    for (size_t i = 0; i < 2; i++) {
+    for (size_t i = 0; i < 3; i++) {
       this->u8g2->drawStr(0, 10 + (i + 1) * 10, content[i].c_str());
     }
   }
